2005ca02afff57ee02c1f1ce466cb3c2c7652a6d
nai golf <niy_golf@hotmail.com>
2018-11-06 00:06:35 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
-const port = process.env.PORT || 5000
+
+app.set('port', (process.env.PORT || 5000)) 
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -48,8 +49,7 @@ request({
 }
 
 
-
-app.listen(app.get('port'), function () {
-  console.log('run at port', app.get('port'))
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
 })
 
